cc9c47df49e50f9bb7cfd88e3c1f5c6c16c87e10
kenneth smith <kennethsmith@kenneths-MacBook-Pro.local>
2022-04-09 18:36:07 -0500

    changes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -12,33 +12,70 @@ function chartcreation(person_id) {
 
         person = personlist[0];
         console.log(person);
-        var otu_ids = person.otu_ids;
-        //var otu_ids_string = otu_ids.map(toNum)
-        console.log(otu_ids)
-        var otu_labels = person.otu_labels;
-        var sample_values = person.sample_values;
         
-        //setup chart components
+        let ids = person.otu_ids;
+        let order_ids = ids.reverse().slice(-10);
+        let otu_ids = []
+        for (var i = 0; i < 10; i++) {
+            order_ids[i]
+            otu_ids.push("OTU "+order_ids[i])
+        }
+        
+        console.log(otu_ids)
+        let otu_labels = person.otu_labels.reverse().slice(-10);
+        let sample_values = person.sample_values.reverse().slice(-10);
+        
+        //setup chart components for horizontal bar chart
         var trace1 = {
-            x: sample_values.slice(0,10).reverse(),
-            y: otu_ids
-            .slice(0,10)
-            // string
-            .reverse(),
-            text: otu_labels.slice(0,10).reverse(),
+            x: sample_values,
+            y: otu_ids,
+            text: otu_labels,
             type: "bar",
             orientation: "h"
         }
 
         let chart_data = [trace1];
 
-        let layout = {
+        var layout = {
             title: "Bar Chart",
-            height: 400,
+            yaxis: { type: 'category'},
+            height: 400
         };
 
         Plotly.newPlot("bar", chart_data, layout)
 
+        //setup chart components for bubble chart
+        var trace2 = {
+            x: ids,
+            y: sample_values,
+            text: otu_labels,
+            type: "scatter",
+            mode: "markers",
+            text: otu_labels,
+            marker: {
+                color: ids,
+                size: sample_values,
+                colorscale: "Electric"
+            },
+        }
+
+        var bub_data = [trace2];
+
+        var bub_layout = {
+            title: "Dashboard",
+            paper_bgcloer:'rgba(0,0,0,0)',
+            plot_bgcloer:'rgba(0,0,0,0)',
+            yaxis: { title: 'sample_values'},
+            yaxis: {range: [0,250]},
+            xaxis: {title: 'otu_ids'},
+            yaxis: {title: 'values'},
+            height: 400,
+            width: 1200,
+            magin: {t:0},
+            
+        };
+
+        Plotly.newPlot("bubble", bub_data, bub_layout)
 
     });
 };
